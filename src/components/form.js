c4a1bfd9644299394ff90f8ac0ee6f2d10283050
xuziwei <xuziwei89@gmail.com>
2018-07-26 03:11:40 +0800

    fix bug

--- src/components/form.js
+++ src/components/form.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import CreditCard from '@material-ui/icons/CreditCard';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import grey from '@material-ui/core/colors/grey';
-import amber from '@material-ui/core/colors/amber';
-import MediaQuery from 'react-responsive';
-import Modal from './modal';
-
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  withStyles,
+  MuiThemeProvider,
+  createMuiTheme
+} from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import CreditCard from "@material-ui/icons/CreditCard";
+import Paper from "@material-ui/core/Paper";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import grey from "@material-ui/core/colors/grey";
+import amber from "@material-ui/core/colors/amber";
+import MediaQuery from "react-responsive";
+import Modal from "./modal";
+import ReactLoading from "react-loading";
+import ReactModal from "react-modal";
 
 const styles = theme => ({
   wrapper: {
@@ -28,9 +33,9 @@ const styles = theme => ({
   },
   root: {
     color: amber[600],
-    '&$checked': {
-      color: amber[500],
-    },
+    "&$checked": {
+      color: amber[500]
+    }
   },
   checked: true,
   flex: {
@@ -47,7 +52,7 @@ const styles = theme => ({
     lineHeight: "0.8rem"
   },
   container: {
-    margin: "2rem",
+    margin: "2rem"
   },
   paperClass: {
     margin: "0 auto",
@@ -57,6 +62,7 @@ const styles = theme => ({
   },
   formControl: {
     margin: "2rem auto",
+    width: "80%"
   },
   inputWrapper: {
     margin: "0.1rem"
@@ -116,27 +122,32 @@ const styles = theme => ({
   },
   margin4: {
     margin: "0 1rem"
+  },
+  justifyContent: {
+    alignItems: "center",
+    display: "flex"
   }
 });
 
 const theme = createMuiTheme({
   palette: {
-    primary: grey,
-  },
+    primary: grey
+  }
 });
 
 class RegisterForm extends React.Component {
   constructor(props) {
-      super(props);
+    super(props);
     this.state = {
-      name: '',
-      id: '',
+      name: "",
+      id: "",
       openModal: false,
       checked: true,
       buttonDisabled: true,
       nameError: true,
-      idError: true
-    }
+      idError: true,
+      openloadingModal: false
+    };
   }
 
   handleOpen = () => {
@@ -144,29 +155,23 @@ class RegisterForm extends React.Component {
     this.setState({
       openModal: true
     });
-  }
+  };
 
-   handleClose = () => {
+  handleClose = () => {
     console.log("关闭Modal");
     this.setState({
       openModal: false
     });
-  }
-
+  };
 
   handleChange = async () => {
     await this.setState({
       checked: !this.state.checked
     });
-    // if(this.state.checked) {
-
-    // } else {
-      
-    // }
     this.canSubmit();
-  }
+  };
 
-  onChangeName = async (e) => {
+  onChangeName = async e => {
     const value = e.target.value;
     if (value.length >= 3) {
       await this.setState({
@@ -174,7 +179,6 @@ class RegisterForm extends React.Component {
         nameError: false
       });
       console.log("name validate", this.state.nameError);
-
     } else {
       await this.setState({
         name: value,
@@ -183,9 +187,9 @@ class RegisterForm extends React.Component {
     }
     this.canSubmit();
     console.log(this.state.checked);
-  }
+  };
 
-  onChangeId = async (e) => {
+  onChangeId = async e => {
     if (this.isCardNo(e.target.value)) {
       await this.setState({
         id: e.target.value,
@@ -201,36 +205,92 @@ class RegisterForm extends React.Component {
     }
     this.canSubmit();
     console.log(this.state.checked);
-  }
+  };
 
-  isCardNo = (id) => /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(id) ? true : false
+  isCardNo = id =>
+    /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(id) ? true : false;
 
-  canSubmit = () => !this.state.idError && !this.state.nameError && this.state.checked ? this.setState({ buttonDisabled: false }) : this.setState({ buttonDisabled: true });
+  canSubmit = () =>
+    !this.state.idError && !this.state.nameError && this.state.checked
+      ? this.setState({ buttonDisabled: false })
+      : this.setState({ buttonDisabled: true });
+
+  submit = async () => {
+    console.log("提交");
+    console.log("");
+    this.setState({ openloadingModal: true });
+    await setTimeout(() => {
+      this.setState({ openloadingModal: false });
+    }, 3000);
+  };
 
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.wrapper}>
         <div className="wrapper__header">
-          <AppBar position="static" style={{ background: "linear-gradient(to bottom,#000000,#000000 45px) #4f0000" }}>
-            <Toolbar >
+          <AppBar
+            position="static"
+            style={{
+              background:
+                "linear-gradient(to bottom,#000000,#000000 45px) #4f0000"
+            }}
+          >
+            <Toolbar>
               <MediaQuery query="(max-device-width: 1024px)">
                 <Typography variant="title" className={classes.flex}>
-                  <img src="https://cdn.2kgames.com/web/nba.2k.com/images/nba2k19_logo_L_2.png" style={{ "margin": "1rem auto", "padding": "1rem", "width": "90%" }} alt="" />
+                  <img
+                    src="https://cdn.2kgames.com/web/nba.2k.com/images/nba2k19_logo_L_2.png"
+                    style={{
+                      margin: "1rem auto",
+                      padding: "1rem",
+                      width: "90%"
+                    }}
+                    alt=""
+                  />
                 </Typography>
               </MediaQuery>
               <MediaQuery query="(min-device-width: 1024px)">
                 <Typography variant="title" className={classes.flex}>
-                  <img src="https://cdn.2kgames.com/web/nba.2k.com/images/nba2k19_logo_L_2.png" style={{ "width": "200px" }} alt="" />
+                  <img
+                    src="https://cdn.2kgames.com/web/nba.2k.com/images/nba2k19_logo_L_2.png"
+                    style={{ width: "200px" }}
+                    alt=""
+                  />
                 </Typography>
-                <Typography variant="title" align="right" style={{ "float": "right" }}>
-                  <a className={classes.link} href="http://wwww.gamepoch.com" target="_blank" rel="noopener noreferrer">星游纪官网 </a>
+                <Typography
+                  variant="title"
+                  align="right"
+                  style={{ float: "right" }}
+                >
+                  <a
+                    className={classes.link}
+                    href="http://wwww.gamepoch.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    星游纪官网{" "}
+                  </a>
                 </Typography>
                 <Typography variant="title" align="right">
-                  <a className={classes.link} href="http://nba2k18.gamepoch.com" target="_blank" rel="noopener noreferrer">PS4国行2K18官网</a>
+                  <a
+                    className={classes.link}
+                    href="http://nba2k18.gamepoch.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    PS4国行2K18官网
+                  </a>
                 </Typography>
                 <Typography variant="title" align="right">
-                  <a className={classes.link} href="https://nba.2k.com/2k19/" target="_blank" rel="noopener noreferrer">NBA2K19官网</a>
+                  <a
+                    className={classes.link}
+                    href="https://nba.2k.com/2k19/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    NBA2K19官网
+                  </a>
                 </Typography>
               </MediaQuery>
             </Toolbar>
@@ -238,75 +298,173 @@ class RegisterForm extends React.Component {
         </div>
         <div className={classes.container}>
           <Paper className={classes.paperClass}>
+            <ReactModal
+              isOpen={this.state.openloadingModal}
+              className="Modal"
+              overlayClassName="Overlay"
+            >
+              <MediaQuery query="(max-device-width: 1024px)">
+                <div
+                  style={{
+                    textAlign: "center",
+                    marginTop: "80%",
+                    fontSize: "1.2rem"
+                  }}
+                >
+                  <div style={{ margin: "10px auto", top: "20%" }}>
+                    <ReactLoading
+                      type={"spin"}
+                      style={{
+                        margin: "0 auto",
+                        height: "4rem",
+                        width: "4rem",
+                        color: "red"
+                      }}
+                    />
+                  </div>
+                  <p>验证成功, 3s后将自动跳转到2k网站</p>
+                </div>
+              </MediaQuery>
+              <MediaQuery query="(min-device-width: 1024px)">
+                <div
+                  style={{
+                    textAlign: "center",
+                    marginTop: "20%",
+                    fontSize: "1.2rem"
+                  }}
+                >
+                  <div style={{ margin: "10px auto", top: "20%" }}>
+                    <ReactLoading
+                      type={"spin"}
+                      style={{
+                        margin: "0 auto",
+                        height: "4rem",
+                        width: "4rem",
+                        color: "red"
+                      }}
+                    />
+                  </div>
+                  <p>验证成功, 3s后将自动跳转到2k网站</p>
+                </div>
+              </MediaQuery>
+            </ReactModal>
             <FormControl className={classes.formControl}>
               <h1>实名认证</h1>
-              <Grid container spacing={16} alignItems="flex-end" className={classes.inputWrapper}>
+              <Grid
+                container
+                spacing={16}
+                alignItems="flex-end"
+                className={classes.inputWrapper}
+              >
                 <Grid item>
                   <AccountCircle />
                 </Grid>
-                <Grid item>
+                <Grid item style={{ width: "80%" }}>
                   <MuiThemeProvider theme={theme}>
-                    <TextField label="公民身份证姓名" onChange={this.onChangeName} error={this.state.nameError} />
+                    <TextField
+                      label="公民身份证姓名"
+                      onChange={this.onChangeName}
+                      error={this.state.nameError}
+                      style={{ width: "100%" }}
+                    />
                   </MuiThemeProvider>
                 </Grid>
               </Grid>
-              <Grid container spacing={16} alignItems="flex-end" className={classes.inputWrapper}>
+              <Grid
+                container
+                spacing={16}
+                alignItems="flex-end"
+                className={classes.inputWrapper}
+              >
                 <Grid item>
                   <CreditCard />
                 </Grid>
-                <Grid item>
+                <Grid item style={{ width: "80%" }}>
                   <MuiThemeProvider theme={theme}>
-                    <TextField label="公民身份证号" onChange={this.onChangeId} error={this.state.idError} />
+                    <TextField
+                      label="公民身份证号(15或者18位)"
+                      onChange={this.onChangeId}
+                      error={this.state.idError}
+                      style={{ width: "100%" }}
+                    />
                   </MuiThemeProvider>
                 </Grid>
               </Grid>
 
-              <FormControlLabel
-                className={classes.center}
-                control={
-                  <Checkbox
-                    classes={{
-                      root: classes.root,
-                      checked: classes.checked,
-                    }}
-                    checked={this.state.checked}
-                    onChange={this.handleChange} />
-                }
-                label="同意我们的隐私规则"
-              />
-              <Button style={{ margin: "-15px 0px 15px 0" }} onClick={this.handleOpen}>
-                查看隐私规则
+              <div className={classes.justifyContent}>
+                <FormControlLabel
+                  className={classes.center}
+                  control={
+                    <Checkbox
+                      classes={{ root: classes.root, checked: classes.checked }}
+                      checked={this.state.checked}
+                      onChange={this.handleChange}
+                    />
+                  }
+                  label="同意隐私规则"
+                />
+
+                <Button onClick={this.handleOpen}>隐私规则</Button>
+              </div>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: "#FFC107", margin: "0 1rem" }}
+                disabled={this.state.buttonDisabled}
+                onClick={this.submit}
+              >
+                提交
               </Button>
-              <Button variant="contained" style={{ "backgroundColor": "#FFC107" }} disabled={this.state.buttonDisabled}>提交</Button>
-              <Modal open={this.state.openModal} handleClose={this.handleClose}/>
+              <Modal
+                open={this.state.openModal}
+                handleClose={this.handleClose}
+              />
             </FormControl>
           </Paper>
         </div>
         <MediaQuery query="(min-device-width: 1024px)">
           <div className={classes.footer}>
             <div className={classes.padding4}>
-              <img className={classes.footerImage} src="https://www.gamepoch.com/images/logo.png" rel="noopener noreferrer"  alt=""/>
+              <img
+                className={classes.footerImage}
+                src="https://www.gamepoch.com/images/logo.png"
+                rel="noopener noreferrer"
+                alt=""
+              />
             </div>
             <div>
               <div className={classes.smallBold}>
                 <p>健康游戏公告：</p>
-                <h5>抵制不良游戏，拒绝盗版游戏。注意自我保护，谨防受骗上当。</h5>
-                <h5>适度游戏益脑，沉迷游戏伤身。合理安排时间，享受健康生活。</h5>
+                <h5>
+                  抵制不良游戏，拒绝盗版游戏。注意自我保护，谨防受骗上当。
+                </h5>
+                <h5>
+                  适度游戏益脑，沉迷游戏伤身。合理安排时间，享受健康生活。
+                </h5>
               </div>
               <div className={classes.margin8}>
-                <p className={classes.small}>本游戏适合12周岁或以上用户，为了您的健康，请合理安排游戏时间。</p>
+                <p className={classes.small}>
+                  本游戏适合12周岁或以上用户，为了您的健康，请合理安排游戏时间。
+                </p>
                 <p>© 2018上海星游纪信息技术有限公司版权所有</p>
               </div>
             </div>
             <div className={classes.footerRight}>
               <div className={classes.flexHorizontal}>
                 <div className={classes.margin4}>
-                  <img className={classes.qrcode} src="https://gamepochblobstorage.blob.core.windows.net/images/NBA2K19/gamepoch-wechat-qrcode.jpg" alt="微信二维码" />
+                  <img
+                    className={classes.qrcode}
+                    src="https://gamepochblobstorage.blob.core.windows.net/images/NBA2K19/gamepoch-wechat-qrcode.jpg"
+                    alt="微信二维码"
+                  />
                   <h5 className={classes.small}>“Gamepoch星游纪”</h5>
                   <h5 className={classes.small}>微信公众号</h5>
                 </div>
                 <div className={classes.margin4}>
-                  <img className={classes.qrcode} src="https://gamepochblobstorage.blob.core.windows.net/images/NBA2K19/gamepoch-weibo-qrcode.png" alt="微博二维码" />
+                  <img
+                    className={classes.qrcode}
+                    src="https://gamepochblobstorage.blob.core.windows.net/images/NBA2K19/gamepoch-weibo-qrcode.png"
+                    alt="微博二维码"
+                  />
                   <h5 className={classes.small}>“Gamepoch星游纪”</h5>
                   <h5 className={classes.small}>新浪微博</h5>
                 </div>
@@ -317,7 +475,11 @@ class RegisterForm extends React.Component {
         <MediaQuery query="(max-device-width: 1024px)">
           <div className={classes.footerSmallDevice}>
             <div className={classes.padding4}>
-              <img className={classes.footerImage} src="https://www.gamepoch.com/images/logo.png"  alt=""/>
+              <img
+                className={classes.footerImage}
+                src="https://www.gamepoch.com/images/logo.png"
+                alt=""
+              />
             </div>
             <div>
               <div className={classes.smallBold}>
@@ -329,16 +491,26 @@ class RegisterForm extends React.Component {
               </div>
 
               <div className={classes.margin8}>
-                <p className={classes.small}>本游戏适合12周岁或以上用户，为了您的健康，请合理安排游戏时间。</p>
+                <p className={classes.small}>
+                  本游戏适合12周岁或以上用户，为了您的健康，请合理安排游戏时间。
+                </p>
                 <div className={classes.footerRight}>
                   <div className={classes.flexHorizontal}>
                     <div className={classes.margin4}>
-                      <img className={classes.qrcode} src="https://gamepochblobstorage.blob.core.windows.net/images/NBA2K19/gamepoch-wechat-qrcode.jpg" alt="微信二维码" />
+                      <img
+                        className={classes.qrcode}
+                        src="https://gamepochblobstorage.blob.core.windows.net/images/NBA2K19/gamepoch-wechat-qrcode.jpg"
+                        alt="微信二维码"
+                      />
                       <h5 className={classes.small}>“Gamepoch星游纪”</h5>
                       <h5 className={classes.small}>微信公众号</h5>
                     </div>
                     <div className={classes.margin4}>
-                      <img className={classes.qrcode} src="https://gamepochblobstorage.blob.core.windows.net/images/NBA2K19/gamepoch-weibo-qrcode.png" alt="微博二维码" />
+                      <img
+                        className={classes.qrcode}
+                        src="https://gamepochblobstorage.blob.core.windows.net/images/NBA2K19/gamepoch-weibo-qrcode.png"
+                        alt="微博二维码"
+                      />
                       <h5 className={classes.small}>“Gamepoch星游纪”</h5>
                       <h5 className={classes.small}>新浪微博</h5>
                     </div>
@@ -355,7 +527,7 @@ class RegisterForm extends React.Component {
 }
 
 RegisterForm.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(RegisterForm);
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,49 +1,66 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Modal from '@material-ui/core/Modal';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Modal from "@material-ui/core/Modal";
+import ReactLoading from "react-loading";
 
 function getModalStyle() {
-  const top = 50 ;
+  const top = 50;
   const left = 50;
 
   return {
     top: `${top}%`,
     left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
+    transform: `translate(-${top}%, -${left}%)`
   };
 }
 
 const styles = theme => ({
   paper: {
-    position: 'absolute',
-    width: theme.spacing.unit * 50,
+    position: "absolute",
+    width: "60%",
     backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing.unit * 4,
+    boxShadow: theme.shadows[10],
+    padding: theme.spacing.unit * 6
   },
+
+  paperSmallDevice: {
+    position: "absolute",
+    width: theme.spacing.unit * 40,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[10],
+    padding: theme.spacing.unit * 6
+  }
 });
 
 class SimpleModal extends React.Component {
   render() {
     const { classes } = this.props;
-    console.log(this.props);
     return (
       <div>
         <Modal
           aria-labelledby="simple-modal-title"
           aria-describedby="simple-modal-description"
-          open={this.props.open}
-          handleClose={this.props.handleClose}
+          open={this.props.open || false}
+          onClose={this.props.handleClose}
         >
           <div style={getModalStyle()} className={classes.paper}>
-            <Typography variant="title" id="modal-title">
-              Text in a modal
+            <Typography
+              variant="title"
+              id="modal-title"
+              align="center"
+              gutterBottom
+            >
+              隐私政策
             </Typography>
-            <Typography variant="subheading" id="simple-modal-description">
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            <Typography variant="body2">
+              上海星游纪信息技术有限公司重视您的隐私。本《隐私政策》就您向我们提供的任何数据，告知您所拥有的选择和我们处理该等数据的常规。
+              使用我们的服务可能涉及在我们的服务（包括我们的网站和流动应用）收集和使用您的数据（在下文「我们所收集的数据及如何收集数据」有所定义）。明白我们如何收集和使用您的数据、您可如何控制收集和使用程序对您是很重要的，因此请仔细阅读本《隐私政策》。
+              您使用我们的服务，即表示同意我们可按照本《隐私政策》（按不时修订）收集、使用和分享您的数据。
+              请注意，为让我们的用户有较佳体验、改善我们的服务、以及为您同意的其他用途，透过我们其中一项服务所收集的个人资料，在不违反用户隐私监控（如有）的前提下，可用于我们的其他服务（包括以汇集或个人化的方式）。
             </Typography>
+
             <SimpleModalWrapped />
           </div>
         </Modal>
@@ -53,7 +70,7 @@ class SimpleModal extends React.Component {
 }
 
 SimpleModal.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 // We need an intermediary variable for handling the recursive nesting.
